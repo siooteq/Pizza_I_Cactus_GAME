61bab99e611761166e2531a808ce27a09183f27b
siooteq <paweel59@gmail.com>
2023-03-30 21:29:34 +0200

    dodanie nowego gracza do gry

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 #stage 2: game rules
 #stage 3: game in play
 #stage 4: game is over
+#stage 5: choose player Men
+#stage 6: choose player Woman
 
+import time
 import pygame
 from random import randint
 pygame.init()
@@ -15,9 +18,9 @@ window = pygame.display.set_mode(resolution)
 
 class Player:
     def __init__(self):
-        self.x_cord = 330 #initial values of coordinates
-        self.y_cord = 330
-        self.image = pygame.transform.scale(pygame.image.load("pawl0.png"), (2*55, 2*74))  # wczytywanie grafiki
+        self.x_cord = 190 #initial values of coordinates
+        self.y_cord = 40
+        self.image = pygame.transform.scale(pygame.image.load("pawl0.png"), (82, 111))  # wczytywanie grafiki
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.speed = 5 #prędkość
@@ -30,12 +33,12 @@ class Player:
         if (keys[pygame.K_a] or keys[pygame.K_LEFT]):
             if self.x_cord > 0 + 35:
                 self.x_cord -= self.speed
-        if (keys[pygame.K_s] or keys[pygame.K_DOWN]):
-            if self.y_cord < r_y - self.height - 10:
-                self.y_cord += self.speed
-        if (keys[pygame.K_w] or keys[pygame.K_UP]):
-            if self.y_cord > 0 + 40:
-                self.y_cord -= self.speed
+        # if (keys[pygame.K_s] or keys[pygame.K_DOWN]):
+        #     if self.y_cord < r_y - self.height - 10:
+        #         self.y_cord += self.speed
+        # if (keys[pygame.K_w] or keys[pygame.K_UP]):
+        #     if self.y_cord > 0 + 40:
+        #         self.y_cord -= self.speed
 
         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
 
@@ -44,11 +47,12 @@ class Player:
 
 class Pizza:
     def __init__(self):
-        self.image = pygame.transform.scale(pygame.image.load("pizza0.png"), (40, 40))  # wczytywanie grafiki
+        self.image = pygame.transform.scale(pygame.image.load("pizza0.png"), (60, 60))  # wczytywanie grafiki
         self.width = self.image.get_width()
         self.height = self.image.get_height()
-        self.x_cord = randint(40,r_x - self.width -40)
-        self.y_cord = randint(40,r_y - self.height -40)
+        self.x_cord = randint(0, 2)*150 + 60
+        #self.x_cord = randint(40, r_x - self.width - 40)
+        self.y_cord = 640
         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
 
     def tick(self):
@@ -60,9 +64,9 @@ class Pizza:
 
 class Cactus:
     def __init__(self):
-        self.image = pygame.transform.scale(pygame.image.load("kaktus0.png"), (2 * 40, 2 * 63))  # wczytywanie grafiki
-        self.x_cord = 40
-        self.y_cord = 40
+        self.image = pygame.transform.scale(pygame.image.load("kaktus0.png"), (60, 90))  # wczytywanie grafiki
+        self.x_cord = randint(0, 2)*150 + 60
+        self.y_cord = 640
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
@@ -73,20 +77,23 @@ class Cactus:
     def draw(self):
         window.blit(self.image, (self.x_cord, self.y_cord))
 
-
-
 ###########################################################   Main   ##############################################
 
-
 def main():
-    stage = 4
+    flag = False
+    flag2 = False
+    stage = 1
     clock = 0
     pizzas = []
     cacti = []  #(more than one cactus)
     player = Player()
+    playera = Player()
+    playera.image = pygame.transform.scale(pygame.image.load("paula0.png"), (82, 111))  # wczytywanie grafiki
     score = 0
     run = True
+    sex = 0
     ###############################################   loading images   ###########################################
+    choose = pygame.image.load("choose.png")
     rules = pygame.image.load("zasady.png")
     start = pygame.image.load("gamestart.png")
     road = pygame.image.load("droga.png")
@@ -102,25 +109,31 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        # stage 1: game start
-        # stage 2: game rules
-        # stage 3: game in play
-        # stage 4: game is over
-
         if stage == 1: #game start
+            if not (keys[pygame.K_SPACE]):
+                flag = True
             window.blit(start, (0, 0))
-            if keys[pygame.K_SPACE]:
-                stage = 3
-            elif keys[pygame.K_r]:
+            if keys[pygame.K_SPACE] and flag == True:
+                stage = 5
+                flag = False
+            if keys[pygame.K_r]:
                 stage = 2
+        #time.sleep(1)
         if stage == 2: #game rules
+            if not (keys[pygame.K_SPACE]):
+                flag = True
             window.blit(rules, (0, 0))
-            if keys[pygame.K_SPACE]:
-                stage = 3
+            if keys[pygame.K_SPACE] and flag == True:
+                stage = 5
+                flag = False
             elif keys[pygame.K_ESCAPE]:
                 stage = 1
 ###################################################   Animation   ##############################################
         if stage == 3: #game in play
+            if sex == 1:
+                player.image = pygame.transform.scale(pygame.image.load("pawl0.png"), (82, 111))  # wczytywanie grafiki
+            elif sex == 2:
+                player.image = pygame.transform.scale(pygame.image.load("paula0.png"), (82, 111))  # wczytywanie grafiki
             window.blit(road, (0, 0))  # road
             if clock >= 2:
                 clock = 0
@@ -128,8 +141,10 @@ def main():
                 cacti.append(Cactus())
             for pizza in pizzas:
                 pizza.tick()
+                pizza.y_cord += -3
             for cactus in cacti:
                 cactus.tick()
+                cactus.y_cord += -3
             for pizza in pizzas:
                 if player.hitbox.colliderect(pizza.hitbox):
                     pizzas.remove(pizza)
@@ -142,19 +157,64 @@ def main():
                 cactus.draw()
             for cactus in cacti:
                 if player.hitbox.colliderect(cactus.hitbox):
-                    #delay
+                    time.sleep(0.5) #delay 0.5s
                     stage = 4
+                    player.x_cord = 190 # new coordinates for new game
+                    player.y_cord = 40
                     cacti.remove(cactus)
             score_text = pygame.font.Font.render(pygame.font.SysFont("arial", 24), f"Wynik: {score}", True, (0, 0, 0))
             window.blit(score_text, (50, 5))
 
 ##############################################   End of Animation   ##############################################
         if stage == 4: #game is over
+            cacti.clear()
+            pizzas.clear()
             window.blit(gameover, (0, 0))
             score_text = pygame.font.Font.render(pygame.font.SysFont("arial", 40), f"Twój wynik to: {score}", True, (105, 5, 22))
             window.blit(score_text, (124, 80))
             if keys[pygame.K_SPACE]:
+                    stage = 3
+                    score = 0
+        if stage == 5: #choose Men
+            if not (keys[pygame.K_SPACE]):
+                flag = True
+            if not (keys[pygame.K_d] or keys[pygame.K_RIGHT] or keys[pygame.K_LEFT] or keys[pygame.K_a]):
+                flag2 = True
+
+            window.blit(choose, (0, 0))
+            player.x_cord = 200
+            player.y_cord = 320
+            player.draw()
+            if ((keys[pygame.K_d] or keys[pygame.K_RIGHT] or keys[pygame.K_LEFT] or keys[pygame.K_a]) and flag2 == True):
+                stage = 6
+                flag2 = False
+            elif keys[pygame.K_SPACE] and flag == True:
                 stage = 3
+                flag = False
+                player.x_cord = 190
+                player.y_cord = 40
+                sex = 1
+
+        if stage == 6: #choose Woman
+            if not (keys[pygame.K_SPACE]):
+                flag = True
+            if not (keys[pygame.K_d] or keys[pygame.K_RIGHT] or keys[pygame.K_LEFT] or keys[pygame.K_a]):
+                flag2 = True
+
+            window.blit(choose, (0,0))
+            playera.x_cord = 200
+            playera.y_cord = 320
+            playera.draw()
+            if ((keys[pygame.K_d] or keys[pygame.K_RIGHT] or keys[pygame.K_LEFT] or keys[pygame.K_a]) and flag2 == True):
+                stage = 5
+                flag2 = False
+            elif (keys[pygame.K_SPACE]) and flag == True:
+                stage = 3
+                flag = False
+                playera.x_cord = 190
+                playera.y_cord = 40
+                sex = 2
+
 
         pygame.display.update()
 

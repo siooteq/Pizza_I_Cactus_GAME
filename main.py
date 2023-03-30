f57ff8d2f00268d8fee8fed2a7cef9853b2d0341
siooteq <paweel59@gmail.com>
2023-03-30 12:33:59 +0200

    poprawiony main na 4 sceny

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
+#stage 1: game start
+#stage 2: game rules
+#stage 3: game in play
+#stage 4: game is over
+
 import pygame
 from random import randint
-
-import os
-import sys
-
-def restart_program():
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
-
 pygame.init()
-
+############################Resolution#####################################
 r_x = 480
 r_y = 640
-rozdz = (r_x, r_y)
-window = pygame.display.set_mode(rozdz)
+resolution = (r_x, r_y)
+window = pygame.display.set_mode(resolution)
+###########################################################################
 
-import pygame
-pygame.init()
 class Player:
     def __init__(self):
-        self.x_cord = 330 #wspolrzedna
+        self.x_cord = 330 #initial values of coordinates
         self.y_cord = 330
         self.image = pygame.transform.scale(pygame.image.load("pawl0.png"), (2*55, 2*74))  # wczytywanie grafiki
         self.width = self.image.get_width()
@@ -28,16 +24,16 @@ class Player:
         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
 
     def tick(self, keys):
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        if (keys[pygame.K_d] or keys[pygame.K_RIGHT]):
             if self.x_cord < r_x - self.width - 35:
                 self.x_cord += self.speed
-        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        if (keys[pygame.K_a] or keys[pygame.K_LEFT]):
             if self.x_cord > 0 + 35:
                 self.x_cord -= self.speed
-        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        if (keys[pygame.K_s] or keys[pygame.K_DOWN]):
             if self.y_cord < r_y - self.height - 10:
                 self.y_cord += self.speed
-        if keys[pygame.K_w] or keys[pygame.K_UP]:
+        if (keys[pygame.K_w] or keys[pygame.K_UP]):
             if self.y_cord > 0 + 40:
                 self.y_cord -= self.speed
 
@@ -46,7 +42,7 @@ class Player:
     def draw(self):
         window.blit(self.image, (self.x_cord, self.y_cord))
 
-class Cash:
+class Pizza:
     def __init__(self):
         self.image = pygame.transform.scale(pygame.image.load("pizza0.png"), (40, 40))  # wczytywanie grafiki
         self.width = self.image.get_width()
@@ -78,143 +74,89 @@ class Cactus:
         window.blit(self.image, (self.x_cord, self.y_cord))
 
 
-def animacja():
-    pass
+
+###########################################################   Main   ##############################################
+
 
 def main():
-
-    dotyk = False
+    stage = 4
     clock = 0
-    monety = []
-    kaktusy = []
-    punkty = 0
-    run = True
-    first_start = False
-    restart_gry = False
-    wynik = 0
-
-    tekst = pygame.font.Font.render(pygame.font.SysFont("arial", 24), f"Wynik: {punkty}", True, (0, 0, 0))
-    nowa_gra = pygame.font.Font.render(pygame.font.SysFont("arial", 24), "Naciśnij spację aby uruchomić nową grę", True, (0, 0, 0))
-    start = pygame.image.load("gamestart.png")
-    tlo = pygame.image.load("tlo.jpg")
-    droga = pygame.image.load("droga.png")
-    koniec = pygame.image.load("gameover.png")
-
+    pizzas = []
+    cacti = []  #(more than one cactus)
     player = Player()
-   
+    score = 0
+    run = True
+    ###############################################   loading images   ###########################################
+    rules = pygame.image.load("zasady.png")
+    start = pygame.image.load("gamestart.png")
+    road = pygame.image.load("droga.png")
+    gameover = pygame.image.load("gameover.png")
+    score_text = pygame.font.Font.render(pygame.font.SysFont("arial", 24), f"Wynik: {score}", True, (0, 0, 0))
+
 
     while run:
-        clock += pygame.time.Clock().tick(60) / 1000 #maksymalnie 60 fps (odswiezanie pętli)
+        clock += pygame.time.Clock().tick(60) / 1000 #max 60 fps (refreshing loop)
         #print(clock)
+        keys = pygame.key.get_pressed()  # values from keyboard
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        keys = pygame.key.get_pressed()
-        window.blit(start, (0, 0))
-        if keys[pygame.K_SPACE]:
-            first_start = True
-        if first_start == True or restart_gry == True :
-            restart_gry = False
+        # stage 1: game start
+        # stage 2: game rules
+        # stage 3: game in play
+        # stage 4: game is over
 
-            ################ TUTAJ JEST POCZĄTEK ANIMACJI
+        if stage == 1: #game start
+            window.blit(start, (0, 0))
+            if keys[pygame.K_SPACE]:
+                stage = 3
+            elif keys[pygame.K_r]:
+                stage = 2
+        if stage == 2: #game rules
+            window.blit(rules, (0, 0))
+            if keys[pygame.K_SPACE]:
+                stage = 3
+            elif keys[pygame.K_ESCAPE]:
+                stage = 1
+###################################################   Animation   ##############################################
+        if stage == 3: #game in play
+            window.blit(road, (0, 0))  # road
             if clock >= 2:
                 clock = 0
-                monety.append(Cash())
-                kaktusy.append(Cactus())
-            for moneta in monety:
-                moneta.tick()
-            for kaktus in kaktusy:
-                kaktus.tick()
-
-
-            for moneta in monety:
-                if player.hitbox.colliderect(moneta.hitbox):
-                    monety.remove(moneta)
-                    punkty += 1
-                    tekst = pygame.font.Font.render(pygame.font.SysFont("arial", 24), f"Wynik: {punkty}", True, (0, 0, 0))
-
-            # for kaktus in kaktusy:
-            #     if player.hitbox.colliderect(kaktus.hitbox):
-            #         wynik = punkty
-            #         dotyk = True
-            #         window.blit(koniec, (0, 0))
-            #         window.blit(pygame.font.Font.render(pygame.font.SysFont("arial", 32), f"Twój wynik to: {wynik}", True,
-            #                                     (130, 52, 40)), (140, 60))
-
-
-            #window.fill((255, 255, 255))
-            window.blit(droga, (0, 0)) #tlo
-            window.blit(tekst, (50, 5))
-
+                pizzas.append(Pizza())
+                cacti.append(Cactus())
+            for pizza in pizzas:
+                pizza.tick()
+            for cactus in cacti:
+                cactus.tick()
+            for pizza in pizzas:
+                if player.hitbox.colliderect(pizza.hitbox):
+                    pizzas.remove(pizza)
+                    score += 1
             player.tick(keys)
             player.draw()
+            for pizza in pizzas:
+                pizza.draw()
+            for cactus in cacti:
+                cactus.draw()
+            for cactus in cacti:
+                if player.hitbox.colliderect(cactus.hitbox):
+                    #delay
+                    stage = 4
+                    cacti.remove(cactus)
+            score_text = pygame.font.Font.render(pygame.font.SysFont("arial", 24), f"Wynik: {score}", True, (0, 0, 0))
+            window.blit(score_text, (50, 5))
 
-
-            for moneta in monety:
-                moneta.draw()
-            for kaktus in kaktusy:
-                kaktus.draw()
-
-            for kaktus in kaktusy:
-                if player.hitbox.colliderect(kaktus.hitbox):
-
-                    wynik = punkty
-                    window.blit(koniec, (0, 0))
-                    window.blit(pygame.font.Font.render(pygame.font.SysFont("arial", 32), f"Twój wynik to: {wynik}", True,
-                                                         (130, 52, 40)), (140, 60))
-
-                    if keys[pygame.K_SPACE]:
-                        kaktusy.remove(kaktus)
-                        restart_gry = True
-                        punkty = 0
-                        tekst = pygame.font.Font.render(pygame.font.SysFont("arial", 24), f"Wynik: {punkty}", True,
-                                                        (0, 0, 0))
-
+##############################################   End of Animation   ##############################################
+        if stage == 4: #game is over
+            window.blit(gameover, (0, 0))
+            score_text = pygame.font.Font.render(pygame.font.SysFont("arial", 40), f"Twój wynik to: {score}", True, (105, 5, 22))
+            window.blit(score_text, (124, 80))
+            if keys[pygame.K_SPACE]:
+                stage = 3
 
         pygame.display.update()
 
-    print(dotyk)
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
-# class Koniec:
-#     def __init__(self):
-#         self.image = pygame.image.load("gameover.png")  # wczytywanie grafiki
-#         self.x_cord = 0
-#         self.y_cord = 0
-#         self.width = self.image.get_width()
-#         self.height = self.image.get_height()
-#
-#
-#
-#     def tick(self):
-#         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
-#
-#     def draw(self):
-#         window.blit(self.image, (self.x_cord, self.y_cord))
-#
-# class Start:
-#     def __init__(self):
-#         self.image = pygame.image.load("droga.png")  # wczytywanie grafiki
-#         self.imageP = pygame.image.load("pizza0.png")  # wczytywanie grafiki
-#         self.imageC = pygame.image.load("kaktus0.png")  # wczytywanie grafiki
-#         self.tekst1 = pygame.font.Font.render(pygame.font.SysFont("arial", 24), "Pizza & Cactus GAME", True, (0, 0, 0))
-#         self.tekst2 = pygame.font.Font.render(pygame.font.SysFont("arial", 24), "Naciśnij spację, aby rozpocząć", True, (0, 0, 0))
-#         self.x_cord = 0
-#         self.y_cord = 0
-#         self.width = self.image.get_width()
-#         self.height = self.image.get_height()
-#
-#
-#
-#     def tick(self):
-#         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
-#
-#     def draw(self):
-#         window.blit(self.image, (self.x_cord, self.y_cord))
